202310a9c1669971c39a99de1c5621460a63938c
shannonshsu <shannonshsu@gmail.com>
2016-08-28 01:29:14 -0700

    gulp works

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+    sass = require('gulp-sass');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+  	port:3306,
+  	proxy:'localhost/yinyin-anchor',
+
+  })
+})
+
+gulp.task('sass', function(){
+	return gulp.src('themes/shannon/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('themes/shannon/css'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+
+gulp.task('watch', ['browserSync'], function(){
+	gulp.watch('themes/shannon/*.scss', ['sass']);
+	gulp.watch('themes/shannon/**/*.php', browserSync.reload);
+	gulp.watch('themes/shannon/js/*.js', browserSync.reload)
+});
+
+
+
